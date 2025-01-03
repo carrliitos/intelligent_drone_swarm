35865076a3ff95330363d782732b784d06882b26
carrliitos <salazar.ten80@gmail.com>
2025-01-02 21:05:03 -0600

    added data folder for data stuff; properly exit system when encountering errors; remove log to unclog

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 from utils import logger
@@ -10,7 +11,8 @@ import cflib
 directory = context.get_context(os.path.abspath(__file__))
 logger_file_name = Path(directory).stem
 logger_name = Path(__file__).stem
-logger = logger.setup_logger(logger_name, f"{directory}/logs/{logger_file_name}.log")
+logger_file = f"{directory}/logs/{logger_file_name}.log"
+logger = logger.setup_logger(logger_name, logger_file)
 
 def main():
   cflib.crtp.init_drivers(enable_debug_driver=False)
@@ -21,6 +23,8 @@ def main():
     le.test_connection()
   except Exception as e:
     logger.error(f"Error: {e}")
+    sys.exit(1)
 
 if __name__ == '__main__':
   main()
+  os.remove(logger_file)
