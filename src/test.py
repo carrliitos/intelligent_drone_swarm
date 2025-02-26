7a125a421d1b17070e865ccdc020d6585b00db6f
carrliitos <salazar.ten80@gmail.com>
2025-02-25 19:44:36 -0600

    some drone flying with roll pitch and yaw values

--- src/test.py
+++ src/test.py
@@ -1,10 +1,17 @@
 import os
 import sys
+import time
 from pathlib import Path
 
 from utils import logger
 from utils import context
-from udp_connection import ESPDrone
+from udp_connection import UDPConnection
+from command_helper import (
+  CommandHelper, 
+  CrazyFlieXProvideable, 
+  CrazyFlieYProvideable, 
+  CrazyFlieDataProvider
+)
 
 import cflib
 
@@ -18,12 +25,41 @@ def main():
   cflib.crtp.init_drivers(enable_debug_driver=False)
   drone_udp = "udp://192.168.43.42:2390"
 
-  le = ESPDrone(drone_udp)
+  le = UDPConnection(drone_udp)
+  
+  x_provider = CrazyFlieXProvideable(x=0.5)
+  y_provider = CrazyFlieYProvideable(y=-0.3)
+ 
+  pitch_provider = CrazyFlieDataProvider(x_provider)
+  roll_provider = CrazyFlieDataProvider(x_provider)
+  yaw_provider = CrazyFlieDataProvider(y_provider)
+  thrust_provider = CrazyFlieDataProvider(y_provider) 
+
+  settings = {"pitchRate": 0.8, "yawRate": 0.6, "maxThrust": 20000}
   try:
+    le.connect()
+    command_helper = CommandHelper(pitch_provider, 
+                                   roll_provider, 
+                                   yaw_provider, 
+                                   thrust_provider, 
+                                   settings)
     while True:
-      le.connect()
-      # le.thrust__gradual(20000)
-      # le.thrust(20000)
+      command_helper.prepare_data()
+      if int(time.time()) % 5 == 0:
+        logger.info(f"Pitch: {command_helper.pitch}, Roll: {command_helper.roll}, Yaw: {command_helper.yaw}, Thrust: {command_helper.thrust}")
+
+      le.thrust__gradual(thrust_limit=30000,
+                         roll=command_helper.roll,
+                         pitch=command_helper.pitch,
+                         yawrate=command_helper.yaw)
+      # le.thrust(thrust=25000, 
+      #           roll=command_helper.roll, 
+      #           pitch=command_helper.pitch, 
+      #           yawrate=command_helper.yaw)
+      time.sleep(0.01)
+  except KeyboardInterrupt:
+    logger.info("Process interrupted. Terminating...")
+    sys.exit(0)
   except Exception as e:
     logger.error(f"Error: {e}")
     sys.exit(1)
--- src/udp_connection.py
+++ src/udp_connection.py
@@ -15,7 +15,7 @@ logger_name = Path(__file__).stem
 logger = logger.setup_logger(logger_name, f"{directory}/logs/{logger_name}.log")
 
 
-class ESPDrone:
+class UDPConnection:
   def __init__(self, link_uri):
     self.link_uri = link_uri
     self._cf = Crazyflie(rw_cache='./cache')
@@ -29,6 +29,8 @@ class ESPDrone:
 
     # Open link to Crazyflie
     self._cf.open_link(link_uri)
+
+    self.thrust_lower_limit = self.thrust = 15000
 
     logger.info(f"Connecting to {self.link_uri}")
 
@@ -86,36 +88,37 @@ class ESPDrone:
       logger.error(f"Error during connection attempt: {e}")
       sys.exit(1)
 
-  def thrust__gradual(self, thrust_limit):
+  def thrust__gradual(self, thrust_limit, roll, pitch, yawrate):
     """
     Testing only. Gradual increase to target thrust limit.
     """
     thrust_mult = 1
-    thrust_step = 100
-    thrust_lower_limit = 10000
-    thrust = thrust_lower_limit
-    roll = 0
-    pitch = 0
-    yawrate = 0
+    thrust_step = 500
 
-    self._cf.commander.send_setpoint(0, 0, 0, 0)
+    if self.thrust > thrust_limit:
+      self.thrust = thrust_limit
 
-    while thrust < thrust_limit:
-      logger.info(f"Current thrust: {thrust}")
+    if self.thrust < thrust_limit:
+      logger.info(f"Current thrust: {self.thrust}")
+      self._cf.commander.send_setpoint(roll, pitch, yawrate, self.thrust)
+      time.sleep(0.001)
+      self.thrust += thrust_step * thrust_mult
+    else:
+      logger.info(f"Thrust limit reached: {thrust_limit}.")
+      thrust = self.thrust
       self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
-      time.sleep(0.1)
-      thrust += thrust_step * thrust_mult
+      time.sleep(0.001)
 
-      if thrust > thrust_limit:
-        thrust = thrust_limit
-
-    logger.info(f"Thrust limit reached: {thrust_limit}.")
-    self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
-
-  def thrust(self, thrust):
+  def _thrust(self, thrust):
     roll = 0
     pitch = 0
     yawrate = 0
+    self._cf.commander.send_setpoint(0, 0, 0, 0)
+    
+    while thrust:
+      self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+
+  def _thrust(self, thrust, roll, pitch, yawrate):
     self._cf.commander.send_setpoint(0, 0, 0, 0)
     
     while thrust:
