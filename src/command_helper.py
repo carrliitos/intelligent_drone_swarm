346779147f8f54e43d424543fa094291cb2ce357
carrliitos <salazar.ten80@gmail.com>
2025-02-25 18:14:47 -0600

    added abstract classes for data provider

---
+++ src/command_helper.py
@@ -0,0 +1,43 @@
+LINEAR_PR = True
+LINEAR_THRUST = True
+
+from abc import ABC, abstractmethod
+
+class CrazyFlieDataProviderProtocol(ABC):
+  @property
+  @abstractmethod
+  def value(self):
+    pass
+
+class CrazyFlieXProvideable:
+  def __init__(self, x: float):
+    self.x = x
+
+class CrazyFlieYProvideable:
+  def __init__(self, y: float):
+    self.y = y
+
+class SimpleXDataProvider(CrazyFlieDataProviderProtocol):
+  def __init__(self, providable: CrazyFlieXProvideable):
+    self.providable = providable
+  
+  @property
+  def value(self):
+    return self.providable.x
+
+class SimpleYDataProvider(CrazyFlieDataProviderProtocol):
+  def __init__(self, providable: CrazyFlieYProvideable):
+    self.providable = providable
+  
+  @property
+  def value(self):
+    return self.providable.y
+
+class CrazyFlieDataProvider:
+  def __init__(self, provider):
+    if isinstance(provider, CrazyFlieXProvideable):
+      self.provider = SimpleXDataProvider(provider)
+    elif isinstance(provider, CrazyFlieYProvideable):
+      self.provider = SimpleYDataProvider(provider)
+    else:
+      raise TypeError("Unsupported provider type")
