7230b65446d23bae51acb964bcf91bf71a59fa81
carrliitos <salazar.ten80@gmail.com>
2025-05-08 20:45:59 -0500

    updated and added cleaning

---
+++ mp_clean.sh
@@ -0,0 +1,7 @@
+mpremote connect /dev/ttyUSB0 fs rm command.py 
+mpremote connect /dev/ttyUSB0 fs rm command_packet.py 
+mpremote connect /dev/ttyUSB0 fs rm esp_drone_udp.py 
+mpremote connect /dev/ttyUSB0 fs rm esp_multidrone_udp.py 
+mpremote connect /dev/ttyUSB0 fs rm esp_now.py 
+
+mpremote connect /dev/ttyUSB0 fs ls
---
+++ mp_update.sh
@@ -0,0 +1,11 @@
+# mpremote connect /dev/ttyUSB0 fs cp src/command.py :
+# mpremote connect /dev/ttyUSB0 fs cp src/command_packet.py :
+# mpremote connect /dev/ttyUSB0 fs cp src/esp_drone_udp.py :
+# mpremote connect /dev/ttyUSB0 fs cp src/esp_multidrone_udp.py :
+# mpremote connect /dev/ttyUSB0 fs cp src/esp_now.py :
+
+mpremote connect /dev/ttyUSB0 fs cp src/old/command.py :
+mpremote connect /dev/ttyUSB0 fs cp src/old/command_packet.py :
+mpremote connect /dev/ttyUSB0 fs cp src/old/esp_drone_udp.py :
+
+mpremote connect /dev/ttyUSB0 fs ls
