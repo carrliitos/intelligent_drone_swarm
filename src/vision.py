86fff671c9543f2b24645cb9a0b9715234e76169
carrliitos <salazar.ten80@gmail.com>
2025-09-16 13:29:14 -0500

    quick bugfix

--- src/vision.py
+++ src/vision.py
@@ -100,8 +100,8 @@ class DetectorRT:
 
     # Rule of thirds: https://web.cecs.pdx.edu/~fliu/papers/ism2011.pdf
     if self.draw_rule_of_thirds:
-      x1, y1 = w // 3, (2 * w) // 3
-      x2, y2 = h // 3, (2 * h) // 3
+      x1, x2 = w // 3, (2 * w) // 3
+      y1, y2 = h // 3, (2 * h) // 3
       cv2.line(frame, (x1, 0), (x1, h), self.grid_color, self.grid_thickness, cv2.LINE_AA)
       cv2.line(frame, (x2, 0), (x2, h), self.grid_color, self.grid_thickness, cv2.LINE_AA)
       cv2.line(frame, (0, y1), (w, y1), self.grid_color, self.grid_thickness, cv2.LINE_AA)
